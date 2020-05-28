6324c821c772146ef9304342c0d4d24bd5323da2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-28 17:08:55 +0800

    Should plot the figures

---
+++ src/plot.py
@@ -0,0 +1,25 @@
+import platform
+import matplotlib.pyplot as plt
+import IPython.display as idisplay
+
+
+def clear_output(wait=True):
+    if platform.system() == 'Windows':
+        plt.close('all')
+        pass
+    else:
+        idisplay.clear_output(wait=wait)
+
+
+def display(objs):
+    if platform.system() == 'Windows':
+        plt.ion()
+        plt.show()
+        plt.imshow(objs)
+        # plt.show(block=False)
+        plt.draw()
+        plt.pause(0.001)
+    else:
+        idisplay.display(objs)
+
+
