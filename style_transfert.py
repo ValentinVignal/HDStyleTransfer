051e8aa8e9a4086820dae0de225d8b95aca865ca
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-28 18:24:13 +0800

    Put other bars and smaller displayed images

--- style_transfert.py
+++ style_transfert.py
@@ -5,7 +5,7 @@ content_path_list, style_path_list = src.data.get_data()
 content_path, style_path, result_path = src.data.get_next_files(content_path_list, style_path_list)
 
 if result_path is not None:
-    image_couple = src.images.load_content_style_img(content_path.as_posix(), style_path.as_posix(), plot_it=True)
+    # image_couple = src.images.load_content_style_img(content_path.as_posix(), style_path.as_posix(), plot_it=True)
 
     extractor = src.StyleContentModel(
         style_layers=src.p.style_layers,
