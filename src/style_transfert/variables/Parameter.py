75aacbd46096fb0ff2901e80a9f7821ef80b6526
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-06-27 20:46:29 +0800

    Can do the combinations of list/tree of parameters !! :tada:

--- src/style_transfert/variables/Parameter.py
+++ src/style_transfert/variables/Parameter.py
@@ -1,33 +1,39 @@
 class Parameter:
     def __init__(self, name, default_value):
+        self.default_value = default_value
         self.name = name
-        self.values = [default_value]
-        self._grid_p = 0
+        self.values = []
+        self._n = 0
 
     @property
-    def grid_p(self):
-        return self._grid_p
+    def n(self):
+        return self._n
 
-    @grid_p.setter
-    def grid_p(self, grid_p):
-        assert(grid_p < self.length)
-        self._grid_p = grid_p
+    @n.setter
+    def n(self, n):
+        assert(n < self.length)
+        self._n = n
 
     @property
     def length(self):
-        return len(self.values)
+        return int(max(len(self.values), 1))
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, item):
+        if len(self.values) == 0:
+            return self.default_value
         return self.values[item]
 
     @property
     def value(self):
-        return self.values[self._grid_p]
+        if len(self.values) == 0:
+            return self.default_value
+        return self.values[self._n]
 
     def update(self, updated_value):
+        self.default_value = updated_value
         self.values = [updated_value]
 
     def set_grid_values(self, grid_values):
@@ -38,7 +44,25 @@ class Parameter:
         """
         self.values = grid_values
 
+    def append_list_value(self, value):
+        if value is None:
+            self.values.append(self.default_value)
+        else:
+            self.values.append(value)
+
     @property
     def num(self):
         return len(self.value)
+
+    def is_constant(self):
+        if self.length == 1:
+            return True
+        first_value = self.values[0]
+        for value in self.values[1:]:
+            if value != first_value:
+                return False
+        return True
+
+
+
 
--- src/style_transfert/variables/ParametersManager.py
+++ src/style_transfert/variables/ParametersManager.py
@@ -1,34 +1,42 @@
+from enum import Enum
+
 from .Parameter import Parameter
 
 
 class ParametersManager:
     def __init__(self):
+        self._n = 0
+        self.parameters_manager_mode = ParametersManagerModes.List
         self._parameters = dict()
-        self._grid_p = 0
 
     @property
-    def grid_p(self):
-        return self._grid_p
+    def n(self):
+        return self._n
 
-    @grid_p.setter
-    def grid_p(self, grid_p):
-        assert(grid_p < self.length)
-        self._grid_p = grid_p
-        # Set to 0
-        for i in range(grid_p):
-            # Increment 1 grip_p times
+    @n.setter
+    def n(self, n):
+        assert(n < self.length)
+        if self.parameters_manager_mode == ParametersManagerModes.List:
+            self._n = n
             for key, value in self._parameters.items():
-                if not value.length == 1:
-                    # This variable can change
-                    if value.grid_p + 1 == value.length:
-                        value.grid_p = 0
-                    else:
-                        # I only have to increment this value
-                        value.grid_p = value.grid_p + 1
-                        break
+                value.n = n
+        elif self.parameters_manager_mode == ParametersManagerModes.Grid:
+            # Set to 0
+            self._n = n
+            for i in range(n):
+                # Increment 1 grip_p times
+                for key, value in self._parameters.items():
+                    if not value.length == 1:
+                        # This variable can change
+                        if value.n + 1 == value.length:
+                            value.n = 0
+                        else:
+                            # I only have to increment this value
+                            value.n = value.n + 1
+                            break
 
     def __setattr__(self, key, value):
-        if key not in ['_parameters', '_grid_p', 'grid_p']:
+        if key not in ['_parameters', 'n', '_n', 'parameters_manager_mode']:
             self.add(key, value)
         else:
             object.__setattr__(self, key, value)
@@ -48,10 +56,14 @@ class ParametersManager:
 
     @property
     def length(self):
-        l = 1
-        for key, value in self._parameters.items():
-            l *= len(value)
-        return l
+        if self.parameters_manager_mode is ParametersManagerModes.List:
+            for key, value in self._parameters.items():
+                return len(value)
+        elif self.parameters_manager_mode is ParametersManagerModes.Grid:
+            l = 1
+            for key, value in self._parameters.items():
+                l *= len(value)
+            return l
 
     def __len__(self):
         return self.length
@@ -62,6 +74,13 @@ class ParametersManager:
     def set_grid_values(self, key, values):
         self._parameters[key].set_grid_values(values)
 
+    def append_list_value(self, obj):
+        for key, value in self._parameters.items():
+            if key in obj:
+                value.append_list_value(obj[key])
+            else:
+                value.append_list_value(value[-1])
+
     def num(self, key):
         return self._parameters[key].num()
 
@@ -69,20 +88,30 @@ class ParametersManager:
         s = '\t\tParameters\n\n'
         s += 'Constant parameters:\n'
         for key, value in self._parameters.items():
-            if value.length == 1:
+            if value.is_constant():
                 s += f'\t{key}: {value.value}\n'
         s += 'Moving Parameters:\n'
         for key, value in self._parameters.items():
-            if value.length > 1:
+            if not value.is_constant():
                 s += f'\t{key}: {value.values}\n'
         with open(path, 'w') as file:
             file.write(s)
 
     def save_current_txt(self, path):
-        s = f'\t\tParameters {self.grid_p}\n\n'
+        s = f'\t\tParameters {self.n}\n\n'
         for key, value in self._parameters.items():
             s += f'\t{key}: {value.value}\n'
         with open(path, 'w') as file:
             file.write(s)
 
+    def list_mode(self):
+        self.parameters_manager_mode = ParametersManagerModes.List
 
+    def grid_mode(self):
+        self.parameters_manager_mode = ParametersManagerModes.Grid
+
+
+class ParametersManagerModes(Enum):
+
+    List = 0
+    Grid = 1
